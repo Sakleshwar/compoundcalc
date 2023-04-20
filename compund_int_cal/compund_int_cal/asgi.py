c03c8d8c1c0b60b93c1c2e1d148a2b82a77582cf
Sakleshwar <sakleshwarmarolix@gmail.com>
2023-04-20 07:07:35 +0530

    adding compound cal

---
+++ compund_int_cal/compund_int_cal/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for compund_int_cal project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'compund_int_cal.settings')
+
+application = get_asgi_application()
